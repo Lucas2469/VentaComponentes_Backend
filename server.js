731d56df3f75c77271c37ebd242c3b088acb955c
joelLopezz <ltj0029618@est.univalle.edu>
2025-09-27 20:16:11 -0400

    subiendo para merge

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -8,7 +9,7 @@ const path = require("path");
 // ================== MIDDLEWARE ==================
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
 }));
 
 // Middleware para servir imágenes
